d5eda2c3c9f8459ca45bb7037bf33ccc7c042a58
Aykan CESUR <aykan.cesur@nesine.com>
2022-10-20 14:00:17 +0300

    Add project files.

---
+++ gRPCClientExample/Program.cs
@@ -0,0 +1,24 @@
+﻿using Grpc.Net.Client;
+using gRPCExample;
+using System;
+using System.Threading.Tasks;
+
+namespace gRPCClientExample
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var messageClient = new Message.MessageClient(channel);
+            Console.WriteLine("Lütfen gönderilecek mesajı giriniz.");
+            var messageResponse = messageClient.GetMessage(new MessageRequest { Message = Console.ReadLine() });
+
+            await Task.Run(async () =>
+            {
+                while (await messageResponse.ResponseStream.MoveNext(new System.Threading.CancellationToken()))
+                    Console.WriteLine($"Gelen mesaj : {messageResponse.ResponseStream.Current.Message}");
+            });
+        }
+    }
+}
